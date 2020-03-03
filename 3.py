5f72bf7d909369c2b8748961c5372cd7f63947c6
Max <am.ra2016@yandex.ru>
2020-03-03 14:57:11 +0300

    return old inventory

---
+++ 3.py
@@ -0,0 +1,1 @@
+print(len('inventory_inventory_'))
--- test.py
+++ test.py
@@ -1,51 +1,81 @@
-import os, dotenv
-import telebot
-
-dotenv.load_dotenv()
-token = os.environ['dev_token']
-bot = telebot.TeleBot(token)
-keyboard = telebot.types.InlineKeyboardMarkup()
-test_button = telebot.types.InlineKeyboardButton(text="test1",
-                                                 callback_data="testing1")
-keyboard.add(test_button)
-test_button = telebot.types.InlineKeyboardButton(text="test2",
-                                                 callback_data="testing2")
-keyboard.add(test_button)
-test_button = telebot.types.InlineKeyboardButton(text="test3",
-                                                 callback_data="exit")
-keyboard.add(test_button)
-keyboard_f_test = telebot.types.ReplyKeyboardMarkup(True)
-keyboard_f_test.row('1', '2', '3')
-print('start')
+import requests
+from bs4 import BeautifulSoup as bs
 
 
-@bot.callback_query_handler(func=lambda call: True)
-def query_handler(call):
-    if 'testing' in call.data:
-        msg = bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=call.data,
-                                    parse_mode='Markdown')
-        msg = bot.edit_message_reply_markup(call.from_user.id, call.message.message_id, reply_markup=keyboard)
+def read_login_password(message):
+    login = ''
+    with open('edu.txt', 'r') as file:
+        data = file.read()
+        a = data.split('\n')
+        for i in a:
+            x = i.split(':')
+            if x[0] == str(message.chat.id):
+                login = x[1].split(' ')[0]
+                password = x[1].split(' ')[1]
+        # if len(login):
+        #     bot.send_message(message.chat.id, 'Сперва авторизируйтесь', reply_markup=keyboard)
+    return login, password
+
+
+def write_login_password(message, bot, keyboard):
+    if ' ' in message.text:
+        with open('edu.txt', 'r+') as f:
+            data = f.read().strip().split('\n')
+            for i in range(len(data)):
+                if data[i].split(':')[0] == str(message.chat.id):
+                    data[i] = ''
+        data = [x.replace('\n', '') for x in data]
+        with open('edu.txt', 'w') as f:
+            f.write(
+                '\n'.join(data).strip() + '\n' + str(message.chat.id) + ':' + str(
+                    message.text.split(' ')[0]) + ' ' + str(
+                    message.text.split()[1]))
+
+        bot.send_message(message.chat.id, 'Вы успешно авторизовались', reply_markup=keyboard)
     else:
-        bot.send_message(call.from_user.id, call.data, reply_markup=keyboard_f_test)
-        # bot.register_next_step_handler_by_chat_id(call.from_user.id, test_f)
+        bot.send_message(message.chat.id, 'Логин и пароль нужно писать через пробел', reply_markup=keyboard)
 
 
-def test_f(message):
-    # bot.send_message(message.chat.id, 'start', reply_markup=keyboard)
-    if message.text in ['1', '2', '3']:
-        bot.send_message(message.chat.id, message.text, reply_markup=keyboard)
-    bot.register_next_step_handler(message, test_f)
+def click_hm(message, bot, keyboard):
+    # try:
+    login, password = read_login_password(message)
+    table = parsing(login, password)
+    s = []
+    for key, val in table.items():
+        s.append(key + ': ' + ' '.join(val))
+    bot.send_message(message.chat.id, s, reply_markup=keyboard)
+    # print(s)
 
 
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id, 'start', reply_markup=keyboard)
+# except:
+#    bot.send_message(message.chat.id, 'Проверьте логин и пароль', reply_markup=keyboard)
 
 
-@bot.message_handler(content_types=['text'])
-def send_text(message):
-    bot.send_message(message.chat.id, 'отправь еще раз')
-    # bot.register_next_step_handler(message, test_f)
+def login_with_requests(session, login, password):
+    payload = {'main_login': login, 'main_password': password}
+    url = 'https://edu.tatar.ru/logon'
+    headers = {'Referer': url}
+    responce = session.post(url,
+                            data=payload,
+                            headers=headers)
 
 
-bot.polling()
+def parsing(login, password):
+    s = requests.Session()
+    s.proxies = {'http': '62.32.90.99:8080', 'http': '195.191.250.38:3128'}
+    login_with_requests(s, login, password)
+    res = s.get('https://edu.tatar.ru/user/diary/term')
+    html = res.text
+    soup = bs(html, 'html.parser')
+    table = soup.find('tbody')
+    subjects = table.find_all('tr')
+    tds = []
+    for x in subjects:
+        tds.append(list(z.text for z in x.find_all('td') if z.text)[:-2])
+    res = {}
+    for td in tds:
+        if td:
+            res[td[0]] = td[1:]
+    return res
+
+
